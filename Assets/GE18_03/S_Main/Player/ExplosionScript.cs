78eeb709d399d9801f718395b73f8ebb70d90194
tankerbibi <166798898+tankerbibi@users.noreply.github.com>
2025-01-21 15:14:35 +0900

    font stage1 stageInfo
    import noto sans

---
+++ Assets/GE18_03/S_Main/Player/ExplosionScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
